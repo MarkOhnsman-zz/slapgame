911a22dab8eda08fe209edd07ab9de20bb2bb788
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2016-09-25 20:59:30 -0600

    Add Mods

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ var Joker = new Villain('Joker', 100, 0, 5, 5, 10, 10, 15)
 var villain = Joker
 var hits = 0
 var items = {
-  gas:new Items("Joker Gas",0.1,"Oh no! HAHAHAHA can't hit HAHAHAHA as HAHAHAHARD!"),
-  gloves:new Items("Kevlar Gloves",1.3,"Tougher Gloves means harder hits"),
-  batarang:new Items("Improved Batarang",1.2,"A new coating makes the Batarang deliver more damage")
+  gas:new Items("Joker Gas",-0.5,"Oh no! HAHAHAHA can't hit HAHAHAHA as HAHAHAHARD!"),
+  gloves:new Items("Kevlar Gloves",0.5,"Tougher Gloves means harder hits"),
+  batarang:new Items("Improved Batarang",0.2,"A new coating makes the Batarang deliver more damage")
 }
 
 function Villain(name, health, pLow, pHigh, kLow, kHigh, sLow, sHigh) {
   this.name = name
   this.health = health
-  this.mods = [ items.gas  ]
   this.attacks = {
     punch: { pLow: pLow, pHigh: pHigh },
     kick: { kLow: kLow, kHigh: kHigh },
@@ -18,7 +17,7 @@ function Villain(name, health, pLow, pHigh, kLow, kHigh, sLow, sHigh) {
   }
 } 
 Villain.prototype.strike = function (max, min) {
-  this.health -= Math.ceil(Math.random() * (max - min) + min)
+  this.health -= (Math.ceil(Math.random() * (max - min) + min))*addMods()
   return this.health
 }
 function onPunch() {
@@ -54,12 +53,12 @@ function onSpecial() {
 function update() {
   var healthElem = document.getElementById('health')
   var hitCounter = document.getElementById('hitCount')
-  var playerName = document.getElementById('villain')
+  var playerName = document.getElementById('villain') 
   var damage = document.getElementById("damage")
   if (villain.health <= 0) {
     villain.health = 0
   }
-  healthElem.innerHTML = villain.health
+  healthElem.innerHTML = Math.ceil(villain.health)
   hitCounter.innerHTML = hits
   playerName.innerHTML = villain.name
   damage.style.width = villain.health+'%'
@@ -67,6 +66,7 @@ function update() {
 function reset() {
   villain.health = 100
   hits = 0
+  villain.mods = []
   update()
 }
 function Villain(name, health, pLow, pHigh, kLow, kHigh, sLow, sHigh) {
@@ -83,14 +83,26 @@ function Items(name, modifier, description){
   this.modifier = modifier;
   this.description = description;
   this.draw = function(){
-
+     return '<div class="item">'+ this.name +': '+this.description +'</div>';
   }
-
 }
 function addMods(){
-  debugger
-  var allMods=0
+  var allMods= 1
   for(var i = 0; i<villain.mods.length; i++){
-    allMods += villain.mods.modifier
+    allMods += villain.mods[i].modifier
 }return allMods;
+}
+
+Joker["mods"] = []
+
+function giveGas(){
+  villain.mods.push(items.gas);
+} 
+
+function giveGloves(){
+  villain.mods.push(items.gloves);
+}
+
+function giveBatarang(){
+  villain.mods.push(items.batarang)
 }
